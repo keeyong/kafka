26ed7237f244f93a48f398b6b72f03bef1368f8b
Keeyong (Max) <keeyonghan@hotmail.com>
2023-07-10 19:46:12 -0700

    Create producer.py

---
+++ producer.py
@@ -0,0 +1,14 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+producer = KafkaProducer(
+   bootstrap_servers=['localhost:9092'],
+   value_serializer=lambda x: dumps(x).encode('utf-8')
+)
+
+for j in range(999):
+   print("Iteration", j)
+   data = {'counter': j}
+   producer.send('topic_test', value=data)
+   sleep(0.5)
