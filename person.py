e466f69757be62996c0ad9770ba78c44dfdc1260
Keeyong (Max) <keeyonghan@hotmail.com>
2023-07-12 11:37:52 -0700

    Update person.py

--- person.py
+++ person.py
@@ -1,5 +1,8 @@
 """
-Pydantic is a Python library for data parsing and validation. It uses the type hinting mechanism of the newer versions of Python (version 3.6 onwards) and validates the types during the runtime. Pydantic defines BaseModel class. It acts as the base class for creating user defined models.
+Pydantic is a Python library for data parsing and validation. 
+It uses the type hinting mechanism of the newer versions of Python (version 3.6 onwards)
+and validates the types during the runtime. Pydantic defines BaseModel class.
+It acts as the base class for creating user defined models.
 """
 from pydantic import BaseModel
 
