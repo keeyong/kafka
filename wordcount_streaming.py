e50d344ee902a41e68041eafe9b5c54e88cae049
Keeyong (Max) <keeyonghan@hotmail.com>
2023-07-12 22:18:18 -0700

    Update wordcount_streaming.py

--- wordcount_streaming.py
+++ wordcount_streaming.py
@@ -11,17 +11,18 @@ if __name__ == "__main__":
         .config("spark.sql.shuffle.partitions", 3) \
         .getOrCreate()
 
+    # READ
     lines_df = spark.readStream \
         .format("socket") \
         .option("host", "localhost") \
         .option("port", "9999") \
         .load()
 
-    # lines_df.printSchema()
-
+    # TRANSFORM
     words_df = lines_df.select(expr("explode(split(value,' ')) as word"))
     counts_df = words_df.groupBy("word").count()
 
+    # SINK
     word_count_query = counts_df.writeStream \
         .format("console") \
         .outputMode("complete") \
